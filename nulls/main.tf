e17598ab4ceabcbb9f7a745ed867ef8694dba7ef
Keran Braich <keran.braich@ibm.com>
2026-09-03 12:21:37 -0700

    try again

--- nulls/main.tf
+++ nulls/main.tf
@@ -18,6 +18,13 @@ variable "instances" {
   type = number
 }
 
+check "intentional_warning" {
+  assert {
+    condition     = false
+    error_message = "Intentional warning diagnostic for testing."
+  }
+}
+
 resource "null_resource" "this" {
   count = var.instances
 
